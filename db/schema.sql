f4adb096923643df581b60f4283f1d8303e7070a
OvernightSolo <sterlingtx75@gmail.com>
2022-09-11 15:00:56 -0500

    added and set up seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,29 +3,27 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-   id INT NOT NULL AUTO_INCREMENT,
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
    name VARCHAR(30) NOT NULL,
-   PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-   id INT AUTO_INCREMENT,
+   id INT AUTO_INCREMENT PRIMARY KEY,
    title VARCHAR(30) NOT NULL,
    salary DECIMAL NOT NULL,
    department_id INT NOT NULL,
-   PRIMARY KEY (id)
    FOREIGN KEY (department_id)
    REFERENCES department(id)
    ON DELETE SET NULL
 );
 
-CREATE TABLE employee (
-   id INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE employees (
+   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
    first_name VARCHAR(30) NOT NULL,
    last_name VARCHAR(30) NOT NULL,
    role_id INT NOT NULL,
-   manager_id INT NOT NULL,
-   FOREIGN KEY (roles_id)
-   REFERENCES employee_role(id),
+   manager_id INT,
+   FOREIGN KEY (manager_id)
+   REFERENCES roles(id),
    ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES
+   ("Accounting"),
+   ("Analytics"),
+   ("Digital"),
+   ("Media");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+   ("Manager", 100000, 4),
+   ("Accountant", 60000, 1),
+   ("Data Analyst", 70000, 2),
+   ("Dig Content Coordinator", 80000, 3);
+
+-- Manager
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES
+   ("Christena", "Garduno", 4);
+
+-- Employees
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+   ("Rachel", "Ford", 2, 5),
+   ("Jared", "Harrington", 3, 5),
+   ("Evelyn", "Maximo", 4, 5);
