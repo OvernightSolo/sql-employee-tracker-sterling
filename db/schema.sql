43c9cbccd33de7fb239cbf582cda2bd5af469625
OvernightSolo <sterlingtx75@gmail.com>
2022-09-11 14:25:55 -0500

    added schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
+
+CREATE TABLE department (
+   id INT NOT NULL AUTO_INCREMENT,
+   name VARCHAR(30) NOT NULL,
+   PRIMARY KEY (id)
+);
+
+CREATE TABLE employee_role (
+   id INT AUTO_INCREMENT,
+   title VARCHAR(30) NOT NULL,
+   salary DECIMAL NOT NULL,
+   department_id INT NOT NULL,
+   PRIMARY KEY (id)
+   FOREIGN KEY (department_id)
+   REFERENCES department(id)
+   ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+   id INT AUTO_INCREMENT NOT NULL,
+   first_name VARCHAR(30) NOT NULL,
+   last_name VARCHAR(30) NOT NULL,
+   role_id INT NOT NULL,
+   manager_id INT NOT NULL,
+   FOREIGN KEY (role_id)
+   REFERENCES employee_role(id),
+   ON DELETE SET NULL
+);
